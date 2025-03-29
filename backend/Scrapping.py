4058e9d61bbc5416b3d23ab7ed180ae7315d6756
focarica <a.vinicius0042@gmail.com>
2025-03-29 20:57:29 -0300

    Implementation of Test01

---
+++ backend/Scrapping.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from utils import Utils
+import requests
+import os
+
+URL = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+
+class Scraper:
+    def __init__(self):
+        self.downloadsFolder = "Downloads"
+        
+        self.response = requests.get(URL)
+        self.soup = BeautifulSoup(self.response.text, "html.parser")
+
+    
+    def donwloadFiles(self):
+        paths = []
+        
+        if not os.path.exists(self.downloadsFolder):            
+            os.mkdir(self.downloadsFolder)
+                        
+        if not os.path.isfile("Downloads/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"):
+            for link in self.soup.find_all("a", attrs={"class":"internal-link"}):
+                if(link.text in ["Anexo I.", "Anexo II."]):
+                    filename = os.path.join(self.downloadsFolder, link['href'].split('/')[-1])
+                    paths.append(filename)
+                    
+                    with open(filename, 'wb') as file:
+                        file.write(requests.get(urljoin(URL, link['href'])).content)
+        
+            Utils.zipFiles(paths, "../RolsProcedimento.zip")        
+            
+    
+Scraper().donwloadFiles()
---
+++ backend/utils.py
@@ -0,0 +1,13 @@
+import zipfile
+
+class Utils:
+    def zipFiles(files: list, folderToZip: str) -> None:
+        with zipfile.ZipFile(folderToZip, 'w') as zipf:
+            for file in files:
+                zipf.write(file, arcname=file.split('/')[-1]) 
+        
+        
+    def unzipFiles(zippedFolder: str, dest: str) -> None: 
+        with zipfile.ZipFile(zippedFolder, 'r') as zipf:
+            zipf.extractall(dest)
+            
