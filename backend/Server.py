9d96adbb2173992a5baa3b1f285be71473b57d9d
focarica <a.vinicius0042@gmail.com>
2025-04-03 16:36:32 -0300

    incomplete server

--- backend/Server.py
+++ backend/Server.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 
 from fastapi import FastAPI, HTTPException, Query
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 
@@ -13,3 +13,24 @@ engine = create_engine(os.getenv('DB_CONNECTION'))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 app = FastAPI()
 
+@app.get("/operadora/{cnpj}")
+def getDetails(cnpj: str):
+    try:
+        with SessionLocal() as session:
+            result = session.execute(text("SELECT (razao_social, nome_fantasia, modalidade, logradouro, numero, complemento, bairro, cidade, endereco_eletronico, representante) FROM public.operadoras_saude WHERE cnpj = :cnp"), {"cnpj":cnpj})
+            operadora = [dict(row) for row in result.mappings()]
+            return {"data": operadora}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.get("operadoras/modalidade/{modalidade}")
+def getOperadorasModalidades(modalidade: str):
+    try:
+        with SessionLocal() as session:
+            result = session.execute(text("SELECT (razao_social, nome_fantasia, modalidade, logradouro, numero, complemento, bairro, cidade, endereco_eletronico, representante) FROM public.operadoras_saude WHERE cnpj = :cnp"), {"cnpj":cnpj})
+            operadora = [dict(row) for row in result.mappings()]
+            return {"data": operadora}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+# ....
